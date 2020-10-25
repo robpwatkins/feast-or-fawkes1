f50f6a2363a2f34993fa1fe5a8187def3f87de07
robpwatkins <robpwatkins@gmail.com>
2020-10-25 13:53:22 -0500

    heyoooooo

--- src/components/Tester.js
+++ src/components/Tester.js
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
+import React, { /* useState */ } from 'react';
 
 const Tester = () => {
-  const [word, setWord] = useState('null');
+  // const [word, setWord] = useState('null');
+
+  const handleClick = () => {
+    fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+        "x-rapidapi-key": "bf932fb55cmsh5fdfe8a4b7b70f6p14096djsnbc2ed101a881"
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data.word))
+    .catch(err => {
+      console.log(err);
+    });
+  }
 
   return (
     <div>
+      <button onClick={handleClick}>CLICK ME!</button>
       <h3>Tester, here.</h3>
     </div>
   )
