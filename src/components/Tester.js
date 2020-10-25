0780ecdd44872400a87626be908a20e9ad2cee36
robpwatkins <robpwatkins@gmail.com>
2020-10-25 14:06:39 -0500

    gotta get an env file going

--- src/components/Tester.js
+++ src/components/Tester.js
@@ -4,13 +4,13 @@ const Tester = () => {
   const [word, setWord] = useState(null);
 
   const handleClick = () => {
-    fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-        "x-rapidapi-key": "bf932fb55cmsh5fdfe8a4b7b70f6p14096djsnbc2ed101a881"
-      }
-    })
+    // fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
+    //   "method": "GET",
+    //   "headers": {
+    //     "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+    //     "x-rapidapi-key": "bf932fb55cmsh5fdfe8a4b7b70f6p14096djsnbc2ed101a881"
+    //   }
+    // })
     .then(response => response.json())
     .then(data => setWord(data.word))
     .catch(err => {
