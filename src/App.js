81255d04528f73fbc2ff2d919e4c429427cc2a44
robpwatkins <robpwatkins@gmail.com>
2020-10-25 13:36:26 -0500

    gonna fetch a work real quick

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Tester from './components/Tester';
 
 function App() {
   return (
     <div className="App">
-
+      <Tester />
     </div>
   );
 }
---
+++ src/components/Tester.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Tester = () => {
+  const [word, setWord] = useState('null');
+
+  return (
+    <div>
+      <h3>Tester, here.</h3>
+    </div>
+  )
+}
+
+export default Tester;
